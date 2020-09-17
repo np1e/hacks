12bc7d3d14f4ba1bdcb37a31be4c31694d14904f
Nick Hafkemeyer <mail@nhafkemeyer.com>
2020-09-17 14:20:09 +0200

    Change structure

--- setup.box/setup_macos.sh
+++ setup.box/setup_macos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install Homebrew package manager
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 if which brew >/dev/null 2>&1 ; then
     BREW_PREFIX=$(brew --prefix)
@@ -15,17 +15,15 @@ if which brew >/dev/null 2>&1 ; then
 fi
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-chsh -s $(which zsh)
-
+if ! which zsh >/dev/null 2>&1 ; then
+    brew install zsh
+    chsh -s $(which zsh)
+fi
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 echo 'source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
 
-# setup dev directory structure and install config files
-mkdir $HOME/dev
-git clone https://github.com/np1e/dotfiles.git $HOME/dev
-ln -s $HOME/dev/dotfiles $HOME/dotfiles
-source $HOME/dev/dotfiles/bootstrap.sh
 
+# install python via pyenv
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 pyenv init -
 pyenv install 2.7
@@ -35,11 +33,39 @@ pyenv rehash
 
 if pip --version; then
     pip install virtualenv
+    pip install --user awscli awsebcli virtualenv pipenv aws-shell
+    pip install --upgrade pip
 fi
 
 if pip3 --version; then
     pip3 install virtualenv
+    pip3 install --user awscli awsebcli virtualenv pipenv aws-shell
+    pip3 install --upgrade pip
 fi
 
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+nvm install node
+
+if [[ ! -d ~/.ssh ]]; then
+    mkdir -p ~/.ssh
+fi
+
+if [[ ! -e ~/.ssh/sneak.keys ]]; then
+    cd ~/.ssh && \
+    wget https://github.com/np1e.keys && \
+    cat *.keys > authorized_keys
+fi
+
+# setup dev directory structure and install config files
+if [[ ! -e $HOME/dev/dotfiles ]]; then
+    git clone https://github.com/np1e/dotfiles.git $HOME/dev/dotfiles
+    ln -s $HOME/dev/dotfiles $HOME/dotfiles
+fi
+cd $HOME/dev/dotfiles && git pull origin master
+chmod u+x bootstrap.sh
+./bootstrap.sh --force
 
 brew cleanup
